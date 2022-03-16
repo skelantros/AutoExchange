1da42e8d1cc5fa539f9960daaba466d943f4649f
skelantros <ad3fful@yandex.ru>
2022-03-16 13:18:03 +0300

    Base fixes

--- base/src/main/scala/ru/skelantros/autoexchange/base/Field.scala
+++ base/src/main/scala/ru/skelantros/autoexchange/base/Field.scala
@@ -3,17 +3,20 @@ package ru.skelantros.autoexchange.base
 case class Field(name: String, typ: Field.Type, relation: Relation, doc: String, default: Option[String]) {
   import Relation._
 
-  def inSchema: String =
-    s""""name": "$name", "type": $typeInSchema,${default.map(v => s"\"default\": $v, ").getOrElse(" ")} "doc": "$doc" """
-
   lazy val typeInSchema: String = relation match {
     case OptArray =>
-      s"[\"null\", {\"type\": \"array\", \"items\": \"$typ\", \"default\": []}]"
+      s"""[\"null\", {\"type\": \"array\", \"items\": \"$typ\", \"default\": []}]"""
     case ReqArray =>
-      s"[{\"type\": \"array\", \"items\": \"$typ\", \"default\": []}]"
+      s"""[{\"type\": \"array\", \"items\": \"$typ\", \"default\": []}]"""
     case Opt =>
-      s"[\"null\", \"$typ\"]"
-    case Req => s"\"$typ\""
+      s"""[\"null\", \"$typ\"]"""
+    case Req => s"""\"$typ\""""
+  }
+
+  lazy val defaultInSchema: Option[String] = default match {
+    case Some(x) => Some(x)
+    case None if relation.isOptional => Some("null")
+    case None => None
   }
 }
 
--- base/src/main/scala/ru/skelantros/autoexchange/base/Relation.scala
+++ base/src/main/scala/ru/skelantros/autoexchange/base/Relation.scala
@@ -1,11 +1,13 @@
 package ru.skelantros.autoexchange.base
 
-sealed trait Relation
+sealed abstract class Relation(val isRequired: Boolean, val isArray: Boolean) {
+  def isOptional: Boolean = !isRequired
+}
 object Relation {
-  case object Opt extends Relation
-  case object Req extends Relation
-  case object OptArray extends Relation
-  case object ReqArray extends Relation
+  case object Opt extends Relation(false, false)
+  case object Req extends Relation(true, false)
+  case object OptArray extends Relation(false, true)
+  case object ReqArray extends Relation(true, true)
   def apply(str: String): Relation = str match {
     case "11" => Req
     case "01" => Opt
